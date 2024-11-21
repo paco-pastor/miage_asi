4d021600d089160692fc7ae5c071479e4a23476d
Paco-Pastor <paco.pastor@hippolyte-rh.fr>
2024-11-21 12:46:58 +0100

    🚧 Create Ue use case

--- UniversiteDomain/UseCases/UeUseCases/Create/CreateUeUseCases.cs
+++ UniversiteDomain/UseCases/UeUseCases/Create/CreateUeUseCases.cs
@@ -1,6 +1,22 @@
+using UniversiteDomain.DataAdapters;
+using UniversiteDomain.DataAdapters.DataAdaptersFactory;
+using UniversiteDomain.Entities;
+
 namespace UniversiteDomain.UseCases.UeUseCases.Create;
 
-public class CreateUeUseCases
+public class CreateUeUseCases(IRepositoryFactory repositoryFactory)
 {
-    // TODO
+        public async Task<Ue> ExecuteAsync(string numeroUe, string intitule)
+    {
+        var ue = new Ue { NumeroUe = numeroUe, Intitule = intitule };
+        return await ExecuteAsync(ue);
+    }
+    public async Task<Ue> ExecuteAsync (Ue ue)
+    {
+        // TODO Check business rules
+        Ue u = await repositoryFactory.UeRepository().CreateAsync(ue);
+        repositoryFactory.UeRepository().SaveChangesAsync().Wait();
+        return u;
+    }
+    
 }
